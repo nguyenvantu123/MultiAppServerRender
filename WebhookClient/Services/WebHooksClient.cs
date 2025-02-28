94675ba413e11cc5a1b0fd7359a16955eae0e33b
tugtsc <tugtsc>
2025-02-28 16:21:11 +0700

    1

--- WebhookClient/Services/WebHooksClient.cs
+++ WebhookClient/Services/WebHooksClient.cs
@@ -2,9 +2,10 @@
 
 public class WebhooksClient(HttpClient client, ILogger<WebhooksClient> _logger)
 {
-    public Task<HttpResponseMessage> AddWebHookAsync(WebhookSubscriptionRequest payload)
+    public Task<HttpResponseMessage> AddWebHookAsync(WebhookSubscriptionRequest payload, string accessToken)
     {
-
+        _logger.LogError("day la access_token trong LoadWebhooks:" + accessToken);
+        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
         return client.PostAsJsonAsync("/api/webhooks", payload);
     }
 
